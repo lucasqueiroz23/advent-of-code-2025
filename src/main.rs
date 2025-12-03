c4ad82928f498fe83165e6075f5ed7fa825fa6c8
Lucas Queiroz <lucas.fn4@gmail.com>
2025-12-03 12:35:26 -0300

    adds solution for day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Eq, PartialEq)]
+enum RotationDirections {
+    L,
+    R,
+    Null,
+}
+
+fn get_input() -> Vec<String> {
+    let mut file = File::open("input").expect("file should exist");
+    let mut contents = String::new();
+    let _ = file.read_to_string(&mut contents);
+
+    contents.split('\n').map(|line| line.to_string()).collect()
+}
+
+fn update_pointer(operation: RotationDirections, mut point: i32, rotation_value: i32) -> i32 {
+    if operation == RotationDirections::R {
+        point += rotation_value;
+    } else {
+        point -= rotation_value;
+        if point < 0 {
+            point += 100;
+        };
+    }
+
+    point % 100
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = get_input();
+    let mut point: i32 = 50;
+    let mut zero_times: i32 = 0;
+
+    for content in contents {
+        let operation = match content.chars().nth(0) {
+            None => break,
+            Some(operation) => operation,
+        };
+
+        let rotation = match operation {
+            'L' => RotationDirections::L,
+            'R' => RotationDirections::R,
+            _ => RotationDirections::Null,
+        };
+
+        if rotation == RotationDirections::Null {
+            break;
+        }
+
+        let rotation_value: i32 = content[1..].parse().expect("value should exist");
+        point = update_pointer(rotation, point, rotation_value);
+
+        if point == 0 {
+            zero_times += 1;
+        }
+    }
+
+    println!("{zero_times}");
 }
